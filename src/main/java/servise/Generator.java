140923bf8bd3b2d63e847c5af6e575b8a04eced9
vadimkirov <vadimkirov@yandex.ru>
2020-06-16 13:21:13 +0300

    final version

--- src/main/java/servise/Generator.java
+++ src/main/java/servise/Generator.java
@@ -2,7 +2,6 @@ package servise;
 
 import model.Product;
 
-import java.text.DecimalFormat;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Random;
@@ -10,7 +9,6 @@ import java.util.Random;
 import static view.PrintData.dataOutput;
 
 public class Generator {
-
 
 
     public static List<Product> generateData (int maxSize){
@@ -30,23 +28,18 @@ public class Generator {
             randomAdd = random.nextInt(i+1);
             String state = "stateOfProduct" + randomAdd;
 
-            DecimalFormat df = new DecimalFormat("####.###");
-            float price = Float.parseFloat(df.format((Math.random() * 5000)));
+            float price = (float) (Math.random() * 5000);
 
             productList.add(new Product(i+1,name,condition,state,price));
-
-
         }
-
         return productList;
     }
 
+    // В один поток, при мультизаписи скорость падает на 30%
     public static void multiGen(int n, int maxSize, String dirName){
-
         for (int i = 0; i < n; i++) {
-            String fileName = "data"+i+".csv";
+            String fileName = "data"+ i +".csv";
             dataOutput(dirName,fileName,generateData(maxSize));
-
         }
     }
 
